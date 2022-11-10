2b8b7a897bf058f3fba538f59194e95cafa15381
schapper <schapper@amazon.com>
2022-11-10 12:57:32 -0800

    Remove accidental commit of binary and check for errors

--- cmd/reee/main.go
+++ cmd/reee/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gogama/reee-evolution/cmd/reee/reeecmd"
 )
 
 func main() {
-	reeecmd.Execute()
+	err := reeecmd.Execute()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
 }
