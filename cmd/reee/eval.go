86545019d8a7f4b4ef63db4406148233f4f1843d
Victor Schappert <victor.schappert@protonmail.com>
2022-11-19 20:55:58 -0800

    Validate rule and group name
    Note that because the communication protocol between the client and the
    daemon is so simple, these values need to be restricted. i.e. They must
    not contain spaces or newlines. They probably don't need to be THIS
    restricted, so another option would be just to prohibit Unicode space
    characters of any kind.

--- cmd/reee/eval.go
+++ cmd/reee/eval.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"github.com/gogama/reee-evolution/log"
 	"github.com/gogama/reee-evolution/protocol"
 	"io"
@@ -14,8 +15,11 @@ type evalCommand struct {
 }
 
 func (cmd *evalCommand) Validate() error {
-	// TODO: Make sure group and rule names don't have spacey characters.
-	return nil
+	err := validateRuleOrGroupName("group", cmd.Group)
+	if err != nil {
+		return err
+	}
+	return validateRuleOrGroupName("rule", cmd.Rule)
 }
 
 func (cmd *evalCommand) Exec(cmdID string, logger log.Printer, ins io.Reader, _ io.Writer, r *bufio.Reader, w *bufio.Writer) error {
@@ -47,5 +51,23 @@ func (cmd *evalCommand) Exec(cmdID string, logger log.Printer, ins io.Reader, _ 
 
 	// TODO: Read back result.
 
+	return nil
+}
+
+// TODO: This should be specific to validating rule and group and should
+// ideally be shared code that daemon can also use.
+func validateRuleOrGroupName(category, name string) error {
+	for i := range name {
+		c := name[i]
+		if 'a' <= c && c <= 'z' ||
+			'A' <= c && c <= 'Z' ||
+			'0' <= c && c <= '9' ||
+			c == '_' || c == '-' {
+			continue
+		} else {
+			return fmt.Errorf("%s contains invalid character '%c'. "+
+				"valid characters are [a-zA-Z0-9_-]", category, c)
+		}
+	}
 	return nil
 }
